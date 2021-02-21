777c8f9964c9dc413a3bcee019d2d5594cae4feb
jennifermendez <jjael8919@gmail.com>
2021-02-20 21:31:22 -0500

    weather app Readme.md file

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-### jjjj, "jjjjj", "jjjjj"
+# WEATHER APP 
+   ## Description
+   Weather-Forecast is a simple and easy to use applicaton to check the daily weather by city name. The features include the actual date,city name, temperature, humidity, wind speed, and a 5 days forecast. This weather app supports global locations and definitely keeps the user updated with useful information about his/her daily weather conditions.
+
+   ## Table of Contents
+   * [Installation](#installation)
+   * [Usage](#usage)
+   * [License](#license)
+   * [Contributing](#contributing)
+   * [Tests](#tests)
+   * [Questions](#questions)
+   
+   ## Installation
+   The Weather-Forecast uses OpenWeatherMap API to get the current weather by city name.
+
+   ## Usage
+   To check the present weather just enter the name of the city and press search.Inmidiately, you will be presented with the weather information of that city along with 5 days forecast
+
+   ## License
+   MIT
+
+   ## Contributing Guidelines
+   no contribution on this project
+  
+   ## Tests
+   browers
+
+   ## Questions
+   jenniferjael
+   <jjael8919@gmail.com>
+   <https://jenniferjael.github.io/Weather/>
+   by email
--- index.js
+++ index.js
@@ -4,68 +4,71 @@ const fs = require("fs");
 
 // TODO: Create an array of questions for user input
 const questions = [];
-function projectQuestions(){
-inquirer
-  .prompt([
-    {
-      type: "input",
-      message: "What is your title project?",
-      name: "titleName",
-    },
-    {
-      type: "input",
-      message: "What is your github username?",
-      name: "githubUsername",
-    },
-    {
-      type: "input",
-      message: "what is your email address?",
-      name: "email",
-    },
-    {
-      type: "input",
-      message: "How can you be reached for more information?",
-      name: "contact",
-    },
-    {
-      type: "input",
-      message: "What is the description of the project?",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "What is the installation process?",
-      name: "installation",
-    },
-    {
-      type: "input",
-      message: "What is the usage?",
-      name: "usage",
-    },
+function projectQuestions() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "What is your title project?",
+        name: "titleName",
+      },
+      {
+        type: "input",
+        message: "What is your github username?",
+        name: "githubName",
+      },
+      {
+        type: "input",
+        message: "what is your email address?",
+        name: "email",
+      },
+      {
+        type: "input",
+        message: "what is the link to your github profile?",
+        name: "githubProfile",
+      },
+      {
+        type: "input",
+        message: "How can you be reached for more information?",
+        name: "contact",
+      },
+      {
+        type: "input",
+        message: "What is the description of the project?",
+        name: "description",
+      },
+      {
+        type: "input",
+        message: "What is the installation process?",
+        name: "installation",
+      },
+      {
+        type: "input",
+        message: "What is the usage?",
+        name: "usage",
+      },
 
-  
-    {
-      type: "list",
-      message: "What license is being used?",
-      name: "license",
-      choices:
-      ['MIT', 'APACHE 2.0', 'BSD 3', 'GPL 3.0', 'None']
-    },
-    {
-      type: "input",
-      message: "What are the contributing guidelines?",
-      name: "guidelines",
-    },
-    {
-      type: "input",
-      message: "What command should be used to run a test?",
-      name: "test",
-    },
-  ])
-  //fs.writeFile(file, data[, options], callback)
-  // TODO: Create a function to write README file
-  .then((responses) => {
-   let profileName = `# ${responses.titleName} 
+      {
+        type: "list",
+        message: "What license is being used?",
+        name: "license",
+        choices: ["MIT", "APACHE 2.0", "BSD 3", "GPL 3.0", "None"],
+      },
+      {
+        type: "input",
+        message: "What are the contributing guidelines?",
+        name: "guidelines",
+      },
+      {
+        type: "input",
+        message: "What command should be used to run a test?",
+        name: "test",
+      },
+    ])
+    //fs.writeFile(file, data[, options], callback)
+    // TODO: Create a function to write README file
+    .then((responses) => {
+      let profileName = `# ${responses.titleName} 
    ## Description
    ${responses.description}
 
@@ -85,17 +88,25 @@ inquirer
 
    ## License
    ${responses.license}
+
+   ## Contributing Guidelines
+   ${responses.guidelines}
   
+   ## Tests
+   ${responses.test}
 
-   ${githubUsername} ${email}`;
-   console.log(profileName);
-   fs.writeFile("README.md",profileName,err=>{
-     if(err) console.error(err);
-   });
-  });
-
+   ## Questions
+   ${responses.githubName}
+   ${responses.email}
+   ${responses.githubProfile}
+   ${responses.contact}`;
+      console.log(profileName);
+      fs.writeFile("README.md", profileName, (err) => {
+        if (err) console.error(err);
+      });
+    });
 }
-projectQuestions()
+projectQuestions();
 
 //TODO: Create a function to initialize app
 function init() {}
